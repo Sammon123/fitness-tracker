59cb0e1a20e76ceaf9daa35f1987720fbbcd0077
Peter Sammon <petersammon2@gmail.com>
2020-07-26 22:34:39 -0400

    initial mongoose/mongodb set-up

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const db = require('./models/workout');
+const Workout = require('./models/workout');
 
 const app = express();
 
@@ -15,6 +15,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb', { useUnifiedTopology: true, useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   res.render('index')
